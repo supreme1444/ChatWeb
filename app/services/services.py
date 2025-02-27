cf2a9b291113d3e78b74e08c59baf6aa513fe491
supreme1444 <107640724+supreme1444@users.noreply.github.com>
2025-02-27 15:35:06 +0300

    Update services.py

--- app/services/services.py
+++ app/services/services.py
@@ -9,3 +9,12 @@ async def services_add_user(hashed_password: str):
 async def services_cheak_user_id(user1, user2):
     if user1 == user2:
         raise HTTPException(status_code=400, detail="You entered yourself, please enter another user.")
+
+
+async def services_cheak_chat_id(chat_id, db: AsyncSession):
+    chat_stmt = select(models.Chat).where(models.Chat.id == chat_id)
+    chat_result = await db.execute(chat_stmt)
+    chat = chat_result.scalars().first()
+
+    if not chat:
+        raise HTTPException(status_code=404, detail="Chat not found")
